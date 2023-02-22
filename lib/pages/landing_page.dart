73c0dd11f913e11dc9e9d3bb1bb3957c4f1bfe16
duyquan <quan.nd00192@sinhvien.hoasen.edu.vn>
2023-02-22 13:07:48 +0700

    Landing Page

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -1,3 +1,6 @@
+import 'package:english_quotes/values/app_assets.dart';
+import 'package:english_quotes/values/app_colors.dart';
+import 'package:english_quotes/values/app_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -9,20 +12,51 @@ class landingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        Expanded(
-            child: Container(
-          color: Colors.red,
-        )),
-        Expanded(
-            child: Container(
-          color: Colors.yellow,
-        )),
-        Expanded(
-            child: Container(
-          color: Colors.green,
-        )),
-      ]),
+      backgroundColor: AppColors.primaryColor,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(children: [
+          Expanded(
+              child: Container(
+                alignment: Alignment.centerLeft,
+                child: Text('Welcome to ',
+              style: AppStyles.h3,),
+          )),
+          Expanded(
+              child: Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Text('English',
+                    style: AppStyles.h2.copyWith(
+                      color: AppColors.blackGrey,
+                      fontWeight: FontWeight.bold
+                    ),),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 30),
+                    child: Text('Quotes',
+                      textAlign: TextAlign.right,
+                      style: AppStyles.h4.copyWith(
+                        height: 0.5,
+                      ),),
+                  )
+                ],
+              ),
+          )),
+          Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 150),
+                child: RawMaterialButton(
+                  shape: CircleBorder(),
+                  fillColor: AppColors.lightBlue,
+                  onPressed: () {},
+                  child: Image.asset(AppAssets.right_Arrow),),
+              )),
+        ]),
+      ),
     );
   }
 }
--- lib/values/app_assets.dart
+++ lib/values/app_assets.dart
@@ -5,7 +5,7 @@
 class AppAssets {
   static final String imgPath = 'assets/images/';
   static final String reload = imgPath + 'reload.png';
-  static final String left_Arrow = imgPath + 'left_arrow.png';
-  static final String right_Arrow = imgPath + 'right_arrow.png';
+  static final String left_Arrow = imgPath + 'left-arrow.png';
+  static final String right_Arrow = imgPath + 'right-arrow.png';
   static final String heart = imgPath + 'heart.png';
 }
